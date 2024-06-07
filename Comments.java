7eb2167861a87789f6cbcc5a92570759a1b5e15d
radheyshyam26 <radheybt2004@gmail.com>
2024-06-07 11:32:58 +0530

    Added Print

---
+++ Comments.java
@@ -0,0 +1,15 @@
+public class Comments {
+    public static void main(String[] args) {
+        //This is single line comment
+        System.out.println("radhey");
+
+        /*Multi-line comment
+          use / and *
+         */
+
+         System.out.println("SHYAM");
+        
+    }
+    
+    
+}
